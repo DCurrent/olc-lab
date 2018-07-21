98b231dd07a79020b50be64be3b2169f25195214
Caskey, Damon V <dc@caskeys.com>
2018-07-21 09:51:28 -0400

    Color swatch
    Initital commit - generate a color swatch.

---
+++ ExampleProject/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+#include "olcConsoleGameEngine.h"
+
+class Example : public olcConsoleGameEngine
+{
+public:
+	Example()
+	{
+		m_sAppName = L"Color Swatch";
+	}
+
+	virtual bool OnUserCreate()
+	{
+		return true;
+	}
+
+	virtual bool OnUserUpdate(float fElapsedTime)
+	{
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		for (int c = 0; c < 16; c++)
+		{
+			Fill(0, c * 6, 5, c * 6 + 5, PIXEL_QUARTER, c);
+		}
+
+		return true;
+	}
+};
+
+int main()
+{
+
+	Example demo;
+
+	demo.ConstructConsole(160, 100, 8, 8);
+	demo.Start();
+
+	return 0;
+}
